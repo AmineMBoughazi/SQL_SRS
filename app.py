a436ece5d0cab7817441202a312ce1386232bd6d
Amine Boughazi <boughaziaminemehdi@gmail.com>
2025-08-19 00:47:48 +0200

    Error handling

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.write("Hello world")
 data = {"a" : [1,2,3], "b" : [4,5,6]}
 df = pd.DataFrame(data)
 
-tab1= st.tabs(["SQL interpreter"])
+tab1,tab2 = st.tabs(["SQL interpreter","SQL exercice"])
 
 with tab1 :
     input_text = st.text_area(label = "Input your SQL query")

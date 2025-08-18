10257f90736c7746c4424bc022b3a730c84616f7
Amine Boughazi <boughaziaminemehdi@gmail.com>
2025-08-19 00:31:06 +0200

    App initialization

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import duckdb
+
+st.write("Hello world")
+
+data = {"a" : [1,2,3], "b" : [4,5,6]}
+df = pd.DataFrame(data)
+
+tab1= st.tabs(["SQL interpreter"])
+
+with tab1 :
+    input_text = st.text_area(label = "Input your SQL query")
+    st.dataframe(df)
+    if input_text :
+        try :
+            user_query_df = duckdb.sql(input_text).df()
+            st.dataframe(user_query_df)
+        except (ValueError,duckdb.duckdb.ParserException) as e :
+            st.write("Votre query n'est pas valide")
+            st.write(e)
